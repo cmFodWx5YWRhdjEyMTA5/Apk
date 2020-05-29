e55a1cb5adf0548652c923d849ce09cdcf5f727b
nab773 <nabhyadwivedi999@gmail.com>
2020-05-29 14:57:21 +0530

    fourteen

---
+++ app/src/main/java/com/solution/idealz/Dashbord/ui/DetailsActivity.java
@@ -0,0 +1,19 @@
+package com.solution.idealz.Dashbord.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.solution.idealz.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+
+
+    }
+}
