8a861641420d3827677e16ab1a1d1bdeb2cd6784
nab773 <nabhyadwivedi999@gmail.com>
2020-05-30 17:22:52 +0530

    22

--- app/src/main/java/com/solution/idealz/Dashbord/ui/DetailsActivity.java
+++ app/src/main/java/com/solution/idealz/Dashbord/ui/DetailsActivity.java
@@ -1,8 +1,10 @@
 package com.solution.idealz.Dashbord.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -18,8 +20,6 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
-
 
         position=getIntent().getStringExtra("position");
         des=findViewById(R.id.des);
